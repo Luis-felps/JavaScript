f0c2594f1b2de58a8f3d0aa0edae48b578493207
Luis Felipe S. Santos <106755307+Luis-felps@users.noreply.github.com>
2023-10-27 08:55:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocaSomPom(){
+    document.querySelector('#tecla_som_pom').play('');
+}
+
+document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla')
+listaDeTeclas[0].onclick = tocaSomPom;
+
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick =
+listaDeTeclas[3].onclick
+
+//document.querySelector('.tecla_pom').onclick = tocaSomPom
+
+ document.querySelector('.tecla_pom').onclick = tocaSomPom ;
