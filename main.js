8b390f82300d3dc1e53aaddac60182735a245e31
Luis Felipe S. Santos <106755307+Luis-felps@users.noreply.github.com>
2023-11-10 09:03:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
-function tocaSomPom(){
-    document.querySelector('#tecla_som_pom').play('');
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio).play('');
+
+    if(elemento === null){
+        alert('Elemento não encontrado');
+    }
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        alert
+    }
 }
 
 document.querySelectorAll('.tecla')
 const listaDeTeclas = document.querySelectorAll('.tecla')
-listaDeTeclas[0].onclick = tocaSomPom;
 
+for (contador < listaDeTeclas.length; contador++){
+
+    const instrumento = listaDeTeclas[contador].classList[1];
+
+    const tecla = listaDeTeclas[contador];
+
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    };
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space'|| evento.code === 'Enter'){
+        tecla.classList.add('ativa');
+    }
+    tecla.onkeyup =function(){
+        tecla.classList.remove('ativa')
+    }
+}
 listaDeTeclas[1].onclick = tocaSomClap;
 listaDeTeclas[2].onclick =
 listaDeTeclas[3].onclick
